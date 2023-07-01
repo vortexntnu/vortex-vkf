240620e5395c7a710404cc1133ece2aac334ba9e
Eirik Kolås <kolaseirik@gmail.com>
2023-07-01 14:57:32 +0200

    added tests for LTI model

---
+++ test/lti_model_test.cpp
@@ -0,0 +1,65 @@
+#include <gtest/gtest.h>
+#include <models/LTI_model.hpp>
+using namespace Models;
+using Eigen::Matrix;
+using Eigen::Vector;
+
+class LTImodelTest : public ::testing::Test {
+protected:
+    static constexpr int n_x{3}, n_y{1}, n_u{2}, n_v{n_x}, n_w{n_y};
+	LTImodelTest()
+	{
+        Matrix<double,n_x,n_x> A;
+        Matrix<double,n_x,n_u> B;
+        Matrix<double,n_y,n_x> C;
+        Matrix<double,n_y,n_u> D;
+        Matrix<double,n_v,n_v> Q;
+        Matrix<double,n_w,n_w> R;
+
+        A << .5, 0, 0,
+              0, 1, 0,
+              0, 0, 0;
+        B << 1, 0, 
+             0, 0, 
+             0, 0;
+        C << 1, 0, 0;	
+        Q << 1, 0, 0,
+             0, 2, 0,
+             0, 0, 3;
+        R << 1;
+
+        model = Models::LTI_model<n_x,n_y,n_u,n_v,n_w>{A,B,C,Q,R};
+	}
+
+    Models::LTI_model<n_x,n_y,n_u,n_v,n_w> model;
+};
+
+TEST_F(LTImodelTest, fIterate)
+{
+    Vector<double,n_x> x0;
+    Vector<double,n_x> x1;
+    Vector<double,n_u> u0;
+    x0 << 1,
+          0,
+          0;
+    x1 << 1.5,
+            0,
+            0;
+    u0 << 1,
+          0;
+
+    ASSERT_EQ(x1, model.f(1ms,x0,u0));
+}
+
+TEST_F(LTImodelTest, hIterate)
+{
+    Vector<double,n_x> x0;
+    Vector<double,n_y> y0;
+
+    x0 << 1,
+          0,
+          0;
+    y0 << 1;
+
+    ASSERT_EQ(y0, model.h(1ms,x0));
+}
