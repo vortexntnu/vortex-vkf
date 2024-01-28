ca9082c227da731ab13b9128a4e28dcfd567db0f
TristanWolfram <tris.wolfram@googlemail.com>
2024-01-28 12:46:27 +0100

    🐛 Refactor PDAF class to be without parameters

--- vortex-filtering/test/pdaf_test.cpp
+++ vortex-filtering/test/pdaf_test.cpp
@@ -8,30 +8,22 @@ using SimplePDAF = vortex::filter::PDAF<vortex::models::ConstantVelocity<2>, vor
 
 TEST(PDAF, init)
 {
-    SimplePDAF pdaf(0.0, 0.0, 0.0);
-    EXPECT_EQ(pdaf.gate_threshold_, 0.0);
-    EXPECT_EQ(pdaf.prob_of_detection_, 0.0);
-    EXPECT_EQ(pdaf.clutter_intensity_, 0.0);
-}
-
-TEST(PDAF, init_with_params)
-{
-    SimplePDAF pdaf(1.0, 0.9, 0.1);
-    EXPECT_EQ(pdaf.gate_threshold_, 1.0);
-    EXPECT_EQ(pdaf.prob_of_detection_, 0.9);
-    EXPECT_EQ(pdaf.clutter_intensity_, 0.1);
+    SimplePDAF pdaf;
 }
 
 
 // testing the get_weights function
 TEST(PDAF, get_weights_is_calculating)
 {
-    SimplePDAF pdaf(2.0, 0.8, 1.0);
+    SimplePDAF pdaf;
+
+    double prob_of_detection = 0.8;
+    double clutter_intensity = 1.0;
 
     vortex::prob::Gauss2d z_pred(Eigen::Vector2d(0.0, 0.0), Eigen::Matrix2d::Identity());
     std::vector<Eigen::Vector2d> meas = {{0.0, 0.0}, {2.0, 1.0}};
 
-    Eigen::VectorXd weights = pdaf.get_weights(meas, z_pred);
+    Eigen::VectorXd weights = pdaf.get_weights(meas, z_pred, prob_of_detection, clutter_intensity);
 
     std::cout << "weights: " << weights << std::endl;
 
@@ -40,12 +32,15 @@ TEST(PDAF, get_weights_is_calculating)
 
 TEST(PDAF, if_no_clutter_first_weight_is_zero)
 {
-    SimplePDAF pdaf(2.0, 0.8, 0.0);
+    SimplePDAF pdaf;
+
+    double prob_of_detection = 0.8;
+    double clutter_intensity = 0.0;
 
     vortex::prob::Gauss2d z_pred(Eigen::Vector2d(0.0, 0.0), Eigen::Matrix2d::Identity());
     std::vector<Eigen::Vector2d> meas = {{0.0, 0.0}, {2.0, 1.0}};
 
-    Eigen::VectorXd weights = pdaf.get_weights(meas, z_pred);
+    Eigen::VectorXd weights = pdaf.get_weights(meas, z_pred, prob_of_detection, clutter_intensity);
 
     std::cout << "weights: " << weights << std::endl;
 
@@ -54,12 +49,15 @@ TEST(PDAF, if_no_clutter_first_weight_is_zero)
 
 TEST(PDAF, weights_are_decreasing_with_distance)
 {
-    SimplePDAF pdaf(2.0, 0.8, 1.0);
+    SimplePDAF pdaf;
+
+    double prob_of_detection = 0.8;
+    double clutter_intensity = 1.0;
 
     vortex::prob::Gauss2d z_pred(Eigen::Vector2d(1.0, 1.0), Eigen::Matrix2d::Identity());
     std::vector<Eigen::Vector2d> meas = {{2.0, 1.0}, {3.0, 1.0}, {4.0, 1.0}};
 
-    Eigen::VectorXd weights = pdaf.get_weights(meas, z_pred);
+    Eigen::VectorXd weights = pdaf.get_weights(meas, z_pred, prob_of_detection, clutter_intensity);
 
     std::cout << "weights: " << weights << std::endl;
 
@@ -71,7 +69,10 @@ TEST(PDAF, weights_are_decreasing_with_distance)
 // testing the get_weighted_average function
 TEST(PDAF, get_weighted_average_is_calculating)
 {
-    SimplePDAF pdaf(2.0, 0.8, 1.0);
+    SimplePDAF pdaf;
+
+    double prob_of_detection = 0.8;
+    double clutter_intensity = 1.0;
 
     vortex::prob::Gauss2d z_pred(Eigen::Vector2d(0.0, 0.0), Eigen::Matrix2d::Identity());
     vortex::prob::Gauss4d x_pred(Eigen::Vector4d(0.0, 0.0, 0.0, 0.0), Eigen::Matrix4d::Identity());
@@ -81,14 +82,23 @@ TEST(PDAF, get_weighted_average_is_calculating)
         vortex::prob::Gauss4d(Eigen::Vector4d(1.0, 1.0, 1.0, 1.0), Eigen::Matrix4d::Identity())
     };
 
-    vortex::prob::Gauss4d weighted_average = pdaf.get_weighted_average(meas, updated_states, z_pred, x_pred);
+    vortex::prob::Gauss4d weighted_average = pdaf.get_weighted_average(
+        meas, 
+        updated_states, 
+        z_pred, x_pred, 
+        prob_of_detection, 
+        clutter_intensity
+    );
 
     std::cout << "weighted average: " << weighted_average.mean() << std::endl;
 }
 
 TEST(PDAF, average_state_is_in_between_prediction_and_measurement_y_axis)
 {
-    SimplePDAF pdaf(2.0, 0.8, 1.0);
+    SimplePDAF pdaf;
+
+    double prob_of_detection = 0.8;
+    double clutter_intensity = 1.0;
 
     vortex::prob::Gauss2d z_pred(Eigen::Vector2d(1.0, 1.0), Eigen::Matrix2d::Identity());
     vortex::prob::Gauss4d x_pred(Eigen::Vector4d(1.0, 1.0, 0.0, 0.0), Eigen::Matrix4d::Identity());
@@ -97,7 +107,14 @@ TEST(PDAF, average_state_is_in_between_prediction_and_measurement_y_axis)
         vortex::prob::Gauss4d(Eigen::Vector4d(1.0, 1.5, 0.0, 0.0), Eigen::Matrix4d::Identity())
     };
 
-    vortex::prob::Gauss4d weighted_average = pdaf.get_weighted_average(meas, updated_states, z_pred, x_pred);
+    vortex::prob::Gauss4d weighted_average = pdaf.get_weighted_average(
+        meas, 
+        updated_states, 
+        z_pred, 
+        x_pred,
+        prob_of_detection,
+        clutter_intensity
+    );
 
     EXPECT_GT(weighted_average.mean()(1), x_pred.mean()(1));
     EXPECT_GT(weighted_average.mean()(1), z_pred.mean()(1));
@@ -109,7 +126,10 @@ TEST(PDAF, average_state_is_in_between_prediction_and_measurement_y_axis)
 
 TEST(PDAF, average_state_is_in_between_prediction_and_measurement_x_axis)
 {
-    SimplePDAF pdaf(2.0, 0.8, 1.0);
+    SimplePDAF pdaf;
+
+    double prob_of_detection = 0.8;
+    double clutter_intensity = 1.0;
 
     vortex::prob::Gauss2d z_pred(Eigen::Vector2d(1.0, 1.0), Eigen::Matrix2d::Identity());
     vortex::prob::Gauss4d x_pred(Eigen::Vector4d(1.0, 1.0, 0.0, 0.0), Eigen::Matrix4d::Identity());
@@ -118,7 +138,14 @@ TEST(PDAF, average_state_is_in_between_prediction_and_measurement_x_axis)
         vortex::prob::Gauss4d(Eigen::Vector4d(1.5, 1.0, 0.0, 0.0), Eigen::Matrix4d::Identity())
     };
 
-    vortex::prob::Gauss4d weighted_average = pdaf.get_weighted_average(meas, updated_states, z_pred, x_pred);
+    vortex::prob::Gauss4d weighted_average = pdaf.get_weighted_average(
+        meas, 
+        updated_states, 
+        z_pred, 
+        x_pred,
+        prob_of_detection,
+        clutter_intensity
+    );
 
     EXPECT_GT(weighted_average.mean()(0), x_pred.mean()(0));
     EXPECT_GT(weighted_average.mean()(0), z_pred.mean()(0));
@@ -130,7 +157,10 @@ TEST(PDAF, average_state_is_in_between_prediction_and_measurement_x_axis)
 
 TEST(PDAF, average_state_is_in_between_prediction_and_measurement_both_axes)
 {
-    SimplePDAF pdaf(2.0, 0.8, 1.0);
+    SimplePDAF pdaf;
+
+    double prob_of_detection = 0.8;
+    double clutter_intensity = 1.0;
 
     vortex::prob::Gauss2d z_pred(Eigen::Vector2d(1.0, 1.0), Eigen::Matrix2d::Identity());
     vortex::prob::Gauss4d x_pred(Eigen::Vector4d(1.0, 1.0, 0.0, 0.0), Eigen::Matrix4d::Identity());
@@ -139,7 +169,14 @@ TEST(PDAF, average_state_is_in_between_prediction_and_measurement_both_axes)
         vortex::prob::Gauss4d(Eigen::Vector4d(1.5, 1.5, 0.0, 0.0), Eigen::Matrix4d::Identity())
     };
 
-    vortex::prob::Gauss4d weighted_average = pdaf.get_weighted_average(meas, updated_states, z_pred, x_pred);
+    vortex::prob::Gauss4d weighted_average = pdaf.get_weighted_average(
+        meas, 
+        updated_states, 
+        z_pred, 
+        x_pred,
+        prob_of_detection,
+        clutter_intensity
+    );
 
     EXPECT_GT(weighted_average.mean()(0), x_pred.mean()(0));
     EXPECT_GT(weighted_average.mean()(0), z_pred.mean()(0));
@@ -158,19 +195,24 @@ TEST(PDAF, average_state_is_in_between_prediction_and_measurement_both_axes)
 TEST(PDAF, apply_gate_is_calculating)
 {
 
+    
+    SimplePDAF pdaf;
+
     double gate_threshold = 1.8;
-    SimplePDAF pdaf(gate_threshold, 0.8, 1.0);
 
     vortex::prob::Gauss2d z_pred(Eigen::Vector2d(0.0, 0.0), Eigen::Matrix2d::Identity());
     std::vector<Eigen::Vector2d> meas = {{0.0, 1.0}, {1.0, 0.0}, {1.0, 1.0},{0.0, 2.0}, {2.0, 0.0}, {2.0, 2.0}};
 
-    auto [inside, outside] = pdaf.apply_gate(meas, z_pred);
+    auto [inside, outside] = pdaf.apply_gate(
+        meas, 
+        z_pred,
+        gate_threshold);
 }
 
 TEST(PDAF, apply_gate_is_separating_correctly)
 {
     double gate_threshold = 2;
-    SimplePDAF pdaf(gate_threshold, 0.8, 1.0);
+    SimplePDAF pdaf;
     
     Eigen::Matrix2d cov;
     cov << 1.0, 0.0,
@@ -179,7 +221,10 @@ TEST(PDAF, apply_gate_is_separating_correctly)
     vortex::prob::Gauss2d z_pred(Eigen::Vector2d(0.0, 0.0), cov);
     std::vector<Eigen::Vector2d> meas = {{0.0, 4.0}, {4.0, 0.0}};
 
-    auto [inside, outside] = pdaf.apply_gate(meas, z_pred);
+    auto [inside, outside] = pdaf.apply_gate(
+        meas,
+        z_pred,
+        gate_threshold);
 
     EXPECT_EQ(inside.size(), 1u);
     EXPECT_EQ(outside.size(), 1u);
@@ -188,8 +233,9 @@ TEST(PDAF, apply_gate_is_separating_correctly)
 
     Gnuplot gp;
     gp << "set xrange [-8:8]\nset yrange [-8:8]\n";
+    gp << "set size ratio -1\n";
     gp << "set style circle radius 0.05\n";
-    gp << "plot '-' with circles title 'Samples' fs transparent solid 1 noborder\n";
+    gp << "plot '-' with circles title 'Samples' linecolor rgb 'red' fs transparent solid 1 noborder\n";
     gp.send1d(meas);
 
     int object_counter = 0;
@@ -197,7 +243,7 @@ TEST(PDAF, apply_gate_is_separating_correctly)
     gp << "set object " << ++object_counter << " circle center " << z_pred.mean()(0) << "," << z_pred.mean()(1) << " size " << 0.05 << " fs empty border lc rgb 'green'\n";
     gp << "replot\n";
 
-    vortex::plotting::Ellipse prediction = vortex::plotting::gauss_to_ellipse(z_pred, 5.991);
+    vortex::plotting::Ellipse prediction = vortex::plotting::gauss_to_ellipse(z_pred, gate_threshold);
 
     gp << "set object " << ++object_counter << " ellipse center " << prediction.x << "," << prediction.y << " size " << prediction.a << "," << prediction.b << " angle " << prediction.angle
         << "fs empty border lc rgb 'cyan'\n";
@@ -208,12 +254,15 @@ TEST(PDAF, apply_gate_is_separating_correctly)
 TEST(PDAF, apply_gate_is_separating_correctly_2)
 {
     double gate_threshold = 2.1;
-    SimplePDAF pdaf(gate_threshold, 0.8, 1.0);
+    SimplePDAF pdaf;
 
     vortex::prob::Gauss2d z_pred(Eigen::Vector2d(0.0, 0.0), Eigen::Matrix2d::Identity());
     std::vector<Eigen::Vector2d> meas = {{0.0, 1.0}, {1.0, 0.0}, {1.0, 1.0},{0.0, 2.0}, {2.0, 0.0}, {2.0, 2.0}};
 
-    auto [inside, outside] = pdaf.apply_gate(meas, z_pred);
+    auto [inside, outside] = pdaf.apply_gate(
+        meas, 
+        z_pred,
+        gate_threshold);
 
     EXPECT_EQ(inside.size(), 5u);
     EXPECT_EQ(outside.size(), 1u);
@@ -223,8 +272,10 @@ TEST(PDAF, apply_gate_is_separating_correctly_2)
 TEST(PDAF, predict_next_state_is_calculating)
 {
 
-    double gate_threshold = 1.0;
-    SimplePDAF pdaf(gate_threshold, 0.8, 1.0);
+    double gate_threshold = 1.12;
+    double prob_of_detection = 0.8;
+    double clutter_intensity = 1.0;
+    SimplePDAF pdaf;
 
     vortex::prob::Gauss4d x_est(Eigen::Vector4d(0.0, 0.0, 0.0, 0.0), Eigen::Matrix4d::Identity());
     std::vector<Eigen::Vector2d> meas = {{0.0, 1.0}, {1.0, 0.0}, {1.0, 1.0},{0.0, 2.0}, {2.0, 0.0}, {2.0, 2.0}};
@@ -232,19 +283,33 @@ TEST(PDAF, predict_next_state_is_calculating)
     auto dyn_model = std::make_shared<vortex::models::ConstantVelocity<2>>(1.0);
     auto sen_model = std::make_shared<vortex::models::IdentitySensorModel<4, 2>>(1.0);
 
-    auto [x_final, inside, outside, x_pred, z_pred, x_updated] = pdaf.predict_next_state(x_est, meas, 1.0, dyn_model, sen_model);
+    auto [x_final, inside, outside, x_pred, z_pred, x_updated] = pdaf.predict_next_state(
+        x_est, 
+        meas, 
+        1.0, 
+        dyn_model, 
+        sen_model,
+        gate_threshold,
+        prob_of_detection,
+        clutter_intensity);
     std::cout << "x_final: " << x_final.mean() << std::endl;
 
     Gnuplot gp;
     gp << "set xrange [-8:8]\nset yrange [-8:8]\n";
+    gp << "set size ratio -1\n";
     gp << "set style circle radius 0.05\n";
-    gp << "plot '-' with circles title 'Samples' fs transparent solid 1 noborder\n";
+    gp << "plot '-' with circles title 'Samples' linecolor rgb 'red' fs transparent solid 1 noborder\n";
     gp.send1d(meas);
 
     int object_counter = 0;
 
     gp << "set object " << ++object_counter << " circle center " << x_est.mean()(0) << "," << x_est.mean()(1) << " size " << 0.05 << "fs empty border lc rgb 'black'\n";
+    gp << "set object " << ++object_counter << " circle center " << x_pred.mean()(0) << "," << x_pred.mean()(1) << " size " << 0.05 << "fs empty border lc rgb 'pink'\n";
     gp << "set object " << ++object_counter << " circle center " << x_final.mean()(0) << "," << x_final.mean()(1) << " size " << 0.05 << " fs empty border lc rgb 'green'\n";
+    gp << "replot\n";
+
+    gp << "set arrow from " << x_est.mean()(0) << "," << x_est.mean()(1) << " to " << x_pred.mean()(0) << "," << x_pred.mean()(1) << " nohead lc rgb 'pink'\n";
+    gp << "set arrow from " << x_est.mean()(0) << "," << x_est.mean()(1) << " to " << x_final.mean()(0) << "," << x_final.mean()(1) << " nohead lc rgb 'green'\n";
     gp << "replot\n";
 
     vortex::plotting::Ellipse gate = vortex::plotting::gauss_to_ellipse(z_pred, gate_threshold);
@@ -258,8 +323,75 @@ TEST(PDAF, predict_next_state_is_calculating)
         vortex::prob::Gauss2d gauss(state.mean().head(2), state.cov().topLeftCorner(2,2));
         vortex::plotting::Ellipse ellipse = vortex::plotting::gauss_to_ellipse(gauss); 
 
-        gp << "set object " << ++object_counter << " ellipse center " << ellipse.x << "," << ellipse.y << " size " << ellipse.a << "," << ellipse.b << " angle " << ellipse.angle << "fs empty border lc rgb 'blue'\n";
+        gp << "set object " << ++object_counter 
+           << " ellipse center " << ellipse.x << "," << ellipse.y 
+           << " size " << ellipse.a << "," << ellipse.b 
+           << " angle " << ellipse.angle 
+           << "fs solid 0.2 noborder front lc rgb 'blue'\n";    
     };
+        gp << "replot\n";
+}
+
+TEST(PDAF, predict_next_state_2)
+{
+
+    double gate_threshold = 2;
+    double prob_of_detection = 0.8;
+    double clutter_intensity = 1.0;
+    SimplePDAF pdaf;
+
+    vortex::prob::Gauss4d x_est(Eigen::Vector4d(1.0, 1.5, -2.0, 0.0), Eigen::Matrix4d::Identity());
+    std::vector<Eigen::Vector2d> meas = {{-3.0, -3.0}, {0.0, 0.0}, {-1.2, 1.5},{-2.0, -2.0}, {2.0, 0.0}, {-1.0, 1.0}};
+
+    auto dyn_model = std::make_shared<vortex::models::ConstantVelocity<2>>(1.0);
+    auto sen_model = std::make_shared<vortex::models::IdentitySensorModel<4, 2>>(0.5);
+
+    auto [x_final, inside, outside, x_pred, z_pred, x_updated] = pdaf.predict_next_state(
+        x_est, 
+        meas, 
+        1.0, 
+        dyn_model, 
+        sen_model,
+        gate_threshold,
+        prob_of_detection,
+        clutter_intensity);
+    std::cout << "x_final: " << x_final.mean() << std::endl;
+
+    Gnuplot gp;
+    gp << "set xrange [-8:8]\nset yrange [-8:8]\n";
+    gp << "set size ratio -1\n";
+    gp << "set style circle radius 0.05\n";
+    gp << "plot '-' with circles title 'Samples' linecolor rgb 'red' fs transparent solid 1 noborder\n";
+    gp.send1d(meas);
+
+    int object_counter = 0;
+
+    gp << "set object " << ++object_counter << " circle center " << x_est.mean()(0) << "," << x_est.mean()(1) << " size " << 0.05 << "fs empty border lc rgb 'black'\n";
+    gp << "set object " << ++object_counter << " circle center " << x_pred.mean()(0) << "," << x_pred.mean()(1) << " size " << 0.05 << "fs empty border lc rgb 'pink'\n";
+    gp << "set object " << ++object_counter << " circle center " << x_final.mean()(0) << "," << x_final.mean()(1) << " size " << 0.05 << " fs empty border lc rgb 'green'\n";
     gp << "replot\n";
 
+    gp << "set arrow from " << x_est.mean()(0) << "," << x_est.mean()(1) << " to " << x_pred.mean()(0) << "," << x_pred.mean()(1) << " nohead lc rgb 'pink'\n";
+    gp << "set arrow from " << x_est.mean()(0) << "," << x_est.mean()(1) << " to " << x_final.mean()(0) << "," << x_final.mean()(1) << " nohead lc rgb 'green'\n";
+    gp << "replot\n";
+
+    vortex::plotting::Ellipse gate = vortex::plotting::gauss_to_ellipse(z_pred, gate_threshold);
+
+    gp << "set object " << ++object_counter << " ellipse center " << gate.x << "," << gate.y << " size " << gate.a << "," << gate.b << " angle " << gate.angle
+        << "fs empty border lc rgb 'cyan'\n";
+
+    for (const auto& state: x_updated)
+    {   
+        vortex::prob::Gauss2d gauss(state.mean().head(2), state.cov().topLeftCorner(2,2));
+        vortex::plotting::Ellipse ellipse = vortex::plotting::gauss_to_ellipse(gauss); 
+
+        gp << "set object " << ++object_counter 
+           << " ellipse center " << ellipse.x << "," << ellipse.y 
+           << " size " << ellipse.a << "," << ellipse.b 
+           << " angle " << ellipse.angle 
+           << "fs solid 0.2 noborder lc rgb 'blue'\n";
+    };
+
+    gp << "replot\n";
+    
 }
