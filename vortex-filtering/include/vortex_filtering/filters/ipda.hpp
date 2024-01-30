58173249d731abf513f7eedb2f4c0f7e604b9054
Sondre Selberg <sondreselberg@gmail.com>
2024-01-30 20:15:48 +0100

    "da er ipda ferdig" -Eirik

--- vortex-filtering/include/vortex_filtering/filters/ipda.hpp
+++ vortex-filtering/include/vortex_filtering/filters/ipda.hpp
@@ -1,44 +1,112 @@
 #ifndef IPDA_HPP
 #define IPDA_HPP
 
-#include <Eigen/Dense>
+#pragma once
 #include <vector>
+#include <Eigen/Dense>
+#include <vortex_filtering/filters/pdaf.hpp>
+#include <vortex_filtering/models/dynamic_model_interfaces.hpp>
+#include <vortex_filtering/models/sensor_model_interfaces.hpp>
 
-class IPDAFilter {
+namespace vortex::filter {
+template<models::concepts::DynamicModelLTV DynModT, models::concepts::SensorModelLTV SensModT>
+class IPDA {
 public:
-  IPDAFilter();
-  double generate_new_probability(std::vector<Eigen::Vector2d> &measurements, double &probability_of_survival, float &probability_of_detection,
-                                  double &standard_deviation, float &lambda);
+    using SensModI = typename SensModT::SensModI;
+    using DynModI = typename DynModT::DynModI;
+    using DynModPtr = std::shared_ptr<DynModI>;
+    using SensModPtr = std::shared_ptr<SensModI>;
+    using EKF = vortex::filter::EKF<DynModI, SensModI>;
+    using Gauss_z = typename SensModI::Gauss_z;
+    using Gauss_x = typename DynModI::Gauss_x;
+    using Vec_z = typename SensModI::Vec_z;
+    using GaussMix = vortex::prob::GaussianMixture<DynModI::N_DIM_x>;
+    using FAKE_PDAF = vortex::filter::PDAF<DynModT, SensModT>;
+    IPDA(){
+        
+    }
+    /// @brief 
+    /// @param measurements Measurements to iterate over
+    /// @param probability_of_survival How likely the object is to survive (Ps)
+    /// @param probability_of_detection How likely the object is to be detected (Pd)
+    /// @param standard_deviation Standard deviation of the measurements (Sk)
+    /// @param lambda Lambda value for the Poisson distribution (Lambda)
+    static double get_existence_probability(const std::vector<Vec_z> &measurements, double probability_of_survival, double last_detection_probability_, double probability_of_detection, double clutter_intensity, Gauss_z &z_pred){
+        double predicted_existence_probability = probability_of_survival * last_detection_probability_; // Finds Ps
 
-private:
-  double last_detection_probability_;
-  double get_last_detection_probability();
-  double sum_of_gaussian_probabilities(std::vector<Eigen::Vector2d> &measurements, double &standard_deviation);
+        double summed = 0;
+        for(const Vec_z &measurement : measurements){
+            summed += z_pred.pfd(measurement);
+        }
+        double l_k = 1 - probability_of_detection + probability_of_detection / clutter_intensity * summed;
+
+        return (l_k * predicted_existence_probability) / (1 - (1 - l_k) * predicted_existence_probability);
+    }
+    static std::tuple<Gauss_x, double, std::vector<Vec_z>, std::vector<Vec_z>, Gauss_x, Gauss_z, std::vector<Gauss_x>> step(
+        const Gauss_x& x_est, 
+        const std::vector<Vec_z>& z_meas, 
+        double timestep, 
+        const DynModPtr& dyn_model, 
+        const SensModPtr& sen_model,
+        double gate_threshold,
+        double prob_of_detection,
+        double prob_of_survival,
+        double survive_est,
+        double clutter_intensity
+        ) const
+    {
+        auto [x_pred, z_pred] = EKF::predict(dyn_model, sen_model, timestep, x_est);
+        auto [inside, outside] = apply_gate(z_meas, z_pred, gate_threshold);
+
+        std::vector<Gauss_x> x_updated;
+        for (const auto& measurement : inside) {
+            x_updated.push_back(EKF::update(sen_model, x_pred, z_pred, measurement));
+        }
+
+        Gauss_x x_final = get_weighted_average(
+            z_meas,
+            x_updated, 
+            z_pred, 
+            x_pred, 
+            prob_of_detection, 
+            clutter_intensity);
+
+        double existence_probability = get_existence_probability(z_meas, prob_of_survival, survive_est, prob_of_detection, clutter_intensity, z_pred);
+        return {x_final, existence_probability, inside, outside, x_pred, z_pred, x_updated};
+    }
+    static std::tuple<std::vector<Vec_z>, std::vector<Vec_z>> apply_gate(
+        const std::vector<Vec_z>& z_meas, 
+        const Gauss_z& z_pred,
+        double gate_threshold
+        ) {
+          FAKE_PDAF pdaf;
+          return pdaf.apply_gate(z_meas, z_pred, gate_threshold);
+        }
+    static Gauss_x get_weighted_average(
+        const std::vector<Vec_z>& z_meas,
+        const std::vector<Gauss_x>& updated_states,
+        const Gauss_z& z_pred,
+        const Gauss_x& x_pred,
+        double prob_of_detection,
+        double clutter_intensity
+        ){
+            FAKE_PDAF pdaf;
+            return pdaf.get_weighted_average(z_meas, updated_states, z_pred, x_pred, prob_of_detection, clutter_intensity);
+        }
+
+    Eigen::VectorXd get_weights(
+        const std::vector<Vec_z>& z_meas,
+        const Gauss_z& z_pred,
+        double prob_of_detection,
+        double clutter_intensity
+        ){
+            FAKE_PDAF pdaf;
+            return pdaf.get_weights(z_meas, z_pred, prob_of_detection, clutter_intensity);
+        }
 };
-
-IPDAFilter::IPDAFilter() { last_detection_probability_ = 0.0; }
-
-/// @brief
-/// @param measurements Measurements to iterate over
-/// @param probability_of_survival How likely the object is to survive (Ps)
-/// @param probability_of_detection How likely the object is to be detected (Pd)
-/// @param standard_deviation Standard deviation of the measurements (Sk)
-/// @param lambda Lambda value for the Poisson distribution (Lambda)
-double IPDAFilter::generate_new_probability(std::vector<Eigen::Vector2d> &measurements, double &probability_of_survival, float &probability_of_detection,
-                                            double &standard_deviation, float &lambda)
-{
-  double predicted_existence_probability = probability_of_survival * last_detection_probability_;
-  double l_k = 1 - probability_of_detection + probability_of_detection / lambda * sum_of_gaussian_probabilities(measurements, standard_deviation);
-
-  return (l_k * predicted_existence_probability) / (1 - (1 - l_k) * predicted_existence_probability);
-}
-double IPDAFilter::sum_of_gaussian_probabilities(std::vector<Eigen::Vector2d> &measurements, double &standard_deviation)
-{
-  double sum = 0.0;
-
-  return sum;
-}
-
-double IPDAFilter::get_last_detection_probability() { return last_detection_probability_; }
-
+}  // namespace vortex::filter
 #endif // IPDA_HPP
+
+
+
+
